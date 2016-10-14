c8cf1126790e642372bfebef742787cac18e4409
Naman Goyal <2015csb1021@iitrpr.ac.in>
2016-10-14 17:09:59 +0530

    Version 1 complete

--- main.cpp
+++ main.cpp
@@ -19,16 +19,9 @@ int main(int argc, char *argv[])
 
 	simulator.reset_proc();
 
-	simulator.fetch();
-	simulator.decode();
-
+	simulator.run_simplesim();
 
 	simulator.write_data_memory();
 
-
-    /*Memory mem_Block(12);
-    mem_Block.Write(0,5);
-    mem_Block.Write(4,-10);
-    cout<<(signed int)mem_Block.Read(4)<<endl;*/
     return 0;
 }
