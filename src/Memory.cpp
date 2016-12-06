39adad2d0f27cbf6c35d44afc3b7c95df5d46e45
Naman Goyal <namangoyal1001@gmail.com>
2016-12-06 17:51:03 +0530

    Added copyright

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -1,3 +1,22 @@
+/*Copyright (C) 2016, IIT Ropar
+ * This file is part of SimpleRISC simulator.
+ *
+ * SimpleRISC simulator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimpleRISC simulator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Naman Goyal (email: 2015csb1021@iitrpr.ac.in)
+ */
+
 #include "Memory.h"
 
 using namespace std;
